cbd6acb54e7982a23673c9165c5ada0ebbeaf821
Ефремов Алексей <main.efremov@gmail.com>
2016-05-30 14:27:40 +0300

    Добавил gitignore

---
+++ SocialControls/UserInfoButton.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SocialControls
+{
+    partial class UserInfoButton
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.avatarPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.avatarPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // avatarPictureBox
+            // 
+            this.avatarPictureBox.Location = new System.Drawing.Point(4, 4);
+            this.avatarPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.avatarPictureBox.Name = "avatarPictureBox";
+            this.avatarPictureBox.Size = new System.Drawing.Size(150, 150);
+            this.avatarPictureBox.TabIndex = 0;
+            this.avatarPictureBox.TabStop = false;
+            // 
+            // UserInfoButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.avatarPictureBox);
+            this.MaximumSize = new System.Drawing.Size(1000, 158);
+            this.MinimumSize = new System.Drawing.Size(492, 158);
+            this.Name = "UserInfoButton";
+            this.Size = new System.Drawing.Size(492, 158);
+            ((System.ComponentModel.ISupportInitialize)(this.avatarPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox avatarPictureBox;
+    }
+}
